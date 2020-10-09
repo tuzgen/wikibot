c1cba61bee363dd46c2fc0533efea2a696070e76
Oğuz Tüzgen <oguztuzgen@gmail.com>
2020-10-09 21:25:47 +0300

    initialize project

---
+++ index.js
@@ -0,0 +1,30 @@
+// set up dependencies
+const Discord = require('discord.js');
+
+// set up discord client
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+// ready prompt
+client.once('ready', () => {
+	console.log('Ready.. with prefix ' + prefix);
+})
+
+// login using bot token
+client.login(token);
+
+// state variables
+client.on('message', (message) => {
+	if (message.content.startsWith("!wikisearch")) {
+		var toSearch = message.content.substr(11);
+	}
+})
+
+// functions
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
